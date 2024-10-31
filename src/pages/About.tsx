9edfd30f4f5100cd35a458caacd2031a38487ed5
Fáusio Paco <fausio18.paco@gmail.com>
2024-10-31 13:56:34 +0200

    added meta tag component for setting meta in the pages

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,6 +1,14 @@
+import Meta from '../components/Meta/Meta';
+
 const About = () => {
   return (
     <>
+      <Meta
+        name="description"
+        title="Go Vegan - About Us"
+        description="We appreciate a sustainable and deliciously conscious gastronomic experience"
+      />
+
       <h1>Hello About</h1>
     </>
   );
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,13 @@
+import Meta from '../components/Meta/Meta';
+
 const Home = () => {
   return (
     <>
+      <Meta
+        name="description"
+        title="Go Vegan - Where Vegan Cuisine Excels Without Sacrificing Taste!"
+        description="Where Vegan Cuisine Excels Without Sacrificing Taste!"
+      />
       <h1>Hello Home</h1>
     </>
   );
