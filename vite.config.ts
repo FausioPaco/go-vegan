684a99d724cb5e0a2a498f9702261e4c70650f21
Fáusio Paco <fausio18.paco@gmail.com>
2024-11-01 13:09:10 +0200

    added path aliases

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,16 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
+import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+      '@assets': path.resolve(__dirname, './src/assets'),
+      '@components': path.resolve(__dirname, './src/components'),
+      '@layouts': path.resolve(__dirname, './src/layouts'),
+      '@utils': path.resolve(__dirname, './src/utils'),
+    },
+  },
 });
