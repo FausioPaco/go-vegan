b1dbc054b6644a9f026777c0569bb8aec4a868d4
Fáusio Paco <fausio18.paco@gmail.com>
2024-12-20 11:48:31 +0200

    added store to the app

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,15 @@ import { BrowserRouter } from 'react-router-dom';
 import '@fontsource-variable/bitter/index.css';
 import './assets/css/global.css';
 import App from './App.tsx';
+import store from '@store/index';
+import { Provider } from 'react-redux';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
-      <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </BrowserRouter>
   </StrictMode>,
 );
