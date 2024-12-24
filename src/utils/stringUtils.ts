a3717f76bc4fb5b6110f714559acd223725803ed
Fáusio Paco <fausio18.paco@gmail.com>
2024-12-24 14:44:11 +0200

    added plurarize function

--- src/utils/stringUtils.ts
+++ src/utils/stringUtils.ts
@@ -3,3 +3,19 @@ export function capitalize(word: string): string {
 
   return `${word.charAt(0).toUpperCase()}${word.slice(1).toLowerCase()}`;
 }
+
+export function pluralizeUnit(value: number, unit: string): string {
+  const plurals: { [key: string]: string } = {
+    g: 'grams',
+    plate: 'plates',
+    unit: 'units',
+    mouse: 'mice',
+    person: 'people',
+  };
+
+  if (value === 1) {
+    return `${value} ${unit}`;
+  }
+
+  return `${value} ${plurals[unit]}`;
+}
