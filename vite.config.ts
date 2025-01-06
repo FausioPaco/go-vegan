4e77dbde914fc4cf15dd7335e045934a7f66a20f
Fáusio Paco <fausio18.paco@gmail.com>
2025-01-06 12:48:37 +0200

    fixed tsconfig files

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react-swc';
 import svgr from 'vite-plugin-svgr';
-import tsconfigPaths from 'vite-tsconfig-paths';
+import path from 'path';
 
 export default defineConfig({
   plugins: [
@@ -15,8 +15,18 @@ export default defineConfig({
       },
       include: '**/*.svg',
     }),
-    tsconfigPaths(),
   ],
+
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+      '@assets': path.resolve(__dirname, './src/assets'),
+      '@components': path.resolve(__dirname, './src/components'),
+      '@layouts': path.resolve(__dirname, './src/layouts'),
+      '@store': path.resolve(__dirname, './src/store'),
+      '@utils': path.resolve(__dirname, './src/utils'),
+    },
+  },
 
   test: {
     environment: 'happy-dom',
